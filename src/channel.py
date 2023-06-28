c7885f2c4f34ec02e1c0d464762713cb150c4891
Igor Larionov <jedialar@gmail.com>
2023-06-28 16:26:12 +0300

    edit_2: fix channel.py and main.py

--- src/channel.py
+++ src/channel.py
@@ -5,20 +5,23 @@ from googleapiclient.discovery import build
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для YouTube-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        """Конструктор класса с атрибутами для данных канала."""
+        self.__channel_id = channel_id
         self.channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
-        self.url = 'https://www.youtube.com/channel/' + self.channel['items'][0]['id']
+        self.url = f'https://www.youtube.com/channel/{channel_id}'
         self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
-        # self.youtube = build('youtube', 'v3', developerKey=api_key). \
-        #     channels().list(id=channel_id, part='snippet,statistics').execute()
+
+    @property
+    def channel_id(self):
+        """Геттер идентификатора YouTube-канала."""
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -26,10 +29,12 @@ class Channel:
 
     @classmethod
     def get_service(cls):
+        """Возвращает объект службы YouTube API для выполнения запросов."""
         api_key: str = os.getenv('YouTube_API')
         return build('youtube', 'v3', developerKey=api_key)
 
     def to_json(self, filename):
+        """Сохраняет в файл значения атрибутов экземпляра класса"""
         data = self.__dict__
         with open(filename, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=2, ensure_ascii=False)
