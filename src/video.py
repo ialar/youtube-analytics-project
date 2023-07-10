b48295ed3ca27044912cc232abebaa58ecb53974
Igor Larionov <jedialar@gmail.com>
2023-07-10 13:17:56 +0300

    edit_4: fix video.py (deleted redundant comment)

--- src/video.py
+++ src/video.py
@@ -6,11 +6,9 @@ class Video:
 
     def __init__(self, video_id: str):
         """Инициализирует экземпляр класса Video."""
-        # super().__init__(channel_id="UC-OVMPlMA3-YCIeg4z5z23A")
         self.__video_id = video_id
         self.video = Channel.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=video_id
-                                                         ).execute()
+                                                         id=video_id).execute()
         self.title = self.video['items'][0]['snippet']['title']
         self.url = f'https://www.youtube.com/watch?v={video_id}'
         self.view_count = int(self.video['items'][0]['statistics']['viewCount'])
